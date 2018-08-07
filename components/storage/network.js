d61b728eceda0cb52d8a36e8a7d910d80be69de6
CodingCarlos <carloshdmn@gmail.com>
2018-08-07 16:26:45 +0200

    Fix gettting db from folders

--- components/storage/network.js
+++ components/storage/network.js
@@ -7,7 +7,8 @@ const Controller = require('./index');
  */
 exports.updateData = function (object) {
 	const file = gcs.bucket(object.bucket).file(object.name);
-	const localFilename = '/tmp/' + object.name;
+	const fileName = object.name.split('/').pop();
+	const localFilename = '/tmp/' + fileName;
 
 	// console.log('File ' + object.name + ' updated! Reading...');
 
@@ -16,12 +17,12 @@ exports.updateData = function (object) {
 		})
 		.then((data) => {
 			// TODO(CodingCarlos): Check data param to prevent errors.
-			// console.log('File "' + object.name + '" read successfully! Saved in "' + localFilename + '". Reading local file...')
+			// console.log('File "' + fileName + '" read successfully! Saved in "' + localFilename + '". Reading local file...')
 			return require(localFilename);
 		})
 		.then((fileData) => {
 			// console.log('Local file read! Executing actions...')
-			return Controller.fileReaded(fileData, object.name)
+			return Controller.fileReaded(fileData, fileName)
 		})
 		.then((data) => {
 			console.log('All actions finished successfully.');
