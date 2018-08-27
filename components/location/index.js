dbec142d201b96dfd16b647e83966610928b6464
CodingCarlos <carloshdmn@gmail.com>
2018-08-27 12:44:57 +0200

    Configure max distance

--- components/location/index.js
+++ components/location/index.js
@@ -1,11 +1,13 @@
 const geolib = require('geolib');
+const Config = require('../config');
 
+const maxDistance = Config.location.maxDistance
 
 /** allow
- *	???
+ *	Given a geopoint, decide if can or not access to the application
  *	@param lat
  *	@param long
- *	@return {Promise} A promise with the people list
+ *	@return {Promise} A promise with the results data
  */
 function allow(lat, long) {
 	return new Promise((resolve, reject) => {
@@ -14,10 +16,16 @@ function allow(lat, long) {
 		    {latitude: 40.412406, longitude: -3.718247}
 		);
 
-		resolve({
-			allow: true,
+		const result = {
+			allow: false,
 			distance: dist
-		});
+		}
+
+		if (maxDistance === 0 || distance <= maxDistance) {
+			result.allow = true;
+		}
+
+		resolve(result);
 	});
 }
 
