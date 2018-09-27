7f0c61709e0fe5f890e39602f64cbe874416f783
Carlos Hernandez <carloshdmn@gmail.com>
2018-09-27 19:04:55 +0200

    hotfix location allow

--- components/location/index.js
+++ components/location/index.js
@@ -16,18 +16,23 @@ const maxDistance = Config.location.maxDistance
  */
 function allow(lat, long) {
 	return new Promise((resolve, reject) => {
-		var dist = geolib.getDistance(
+		var distance = geolib.getDistance(
 		    {latitude: lat, longitude: long},
 		    {latitude: 40.412406, longitude: -3.718247}
 		);
 
 		const result = {
 			allow: false,
-			distance: dist
+			distance: distance
 		};
+
+		// console.log('Detected distance: ' + distance)
+		// console.log('Maximum allowed distance: ' + maxDistance)
 
 		if (maxDistance === 0 || distance <= maxDistance) {
 			result.allow = true;
+		} else {
+			console.warn('Trying to access ' + distance + ' meters away from Campus.');
 		}
 
 		resolve(result);
